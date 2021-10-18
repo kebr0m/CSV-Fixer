a1b01a0cd0d5bd4c3b55e7d6e792a6c53a681fce
Nathan Kebrom <89224345+kebr0m@users.noreply.github.com>
2021-10-17 22:11:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-''' Short Program t that would search for empty columns in data-sets we would receive and conditionally add the content of the line associated with the empty column to
+''' Short Program that would search for empty columns in data-sets we would receive and conditionally add the content of the line associated with the empty column to
  the line above. The reason being is that the data-sets we would receive were in PDF format and had indentation within address columns.The conversion to CSV would cause error in
 our data format, as it would interpret indentations as new lines. '''
 
